b896386c886a0088bd8627c89f88a33574d32117
Shiun Murata <purpleclouds0916@gmail.com>
2021-11-30 14:42:03 +0900

    本番環境で画面が表示されないエラーの修正

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("/api/hello", (req, res) => {
   res.send({ express: "Hello From Express" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 app.post("/api/world", (req, res) => {
   console.log(req.body);
